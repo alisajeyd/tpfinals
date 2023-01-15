36c36ef6c2784075bc035bff014f60687ab1e72a
Allyssa Jade Taruc <116567904+alisajeyd@users.noreply.github.com>
2023-01-15 19:18:46 +0800

    Add files via upload

---
+++ JOptionPane.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package market;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Jade
+ */
+class JOptionPane {
+
+    static boolean showConfirmDialog(JFrame frame, String confirm_to_Exit, boolean YES_NO_OPTION) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
